e70d894755087a5fcf4ebb3185df2e2370682d30
PedSemp <pedguisem@alu.edu.gva.es>
2025-01-31 13:14:17 +0100

    Citas

---
+++ src/Cita/Cita.java
@@ -0,0 +1,40 @@
+package Cita;
+//Pedro Guillo
+import java.util.Date;
+
+public class Cita {
+    protected int ID_cita;
+    protected int fecha_cita;
+    protected int hora_cita;
+
+    public Cita(int ID_cita,int fecha_cita, int hora_cita) {
+        this.ID_cita = ID_cita;
+        this.fecha_cita = fecha_cita;
+        this.hora_cita = hora_cita;
+    }
+
+    public int getID_cita() {
+        return ID_cita;
+    }
+
+    public void setID_cita(int ID_cita) {
+        this.ID_cita = ID_cita;
+    }
+
+    public int getFecha_cita() {
+        return fecha_cita;
+    }
+
+    public void setFecha_cita(int fecha_cita) {
+        this.fecha_cita = fecha_cita;
+    }
+
+    public int getHora_cita() {
+        return hora_cita;
+    }
+
+    public void setHora_cita(int hora_cita) {
+        this.hora_cita = hora_cita;
+    }
+
+}
---
+++ src/Cita/MainCita.java
@@ -0,0 +1,47 @@
+package Cita;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainCita {
+
+    public static void main(String[] args) {
+        int ID_cita=0;
+        int fecha_cita=0;
+        int hora_cita=0;
+        Scanner scanner=new Scanner(System.in);
+        boolean continuar=true;
+
+        do {
+
+            try {
+                System.out.print("Introduce el ID de la cita:");
+                ID_cita = scanner.nextInt();
+                continuar = false;
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error.Debes poner solo numeros");
+                scanner.nextLine();
+
+            }
+            try{
+                System.out.print("Introduce la fecha de la cita:");
+                fecha_cita= scanner.nextInt();
+                continuar=false;
+            }catch (InputMismatchException e){
+                System.out.println("Error.Debes poner solo numeros");
+                scanner.nextLine();
+            }
+            try{
+                System.out.print("Introduce la hora de la cita");
+                hora_cita= scanner.nextInt();
+                continuar=false;
+
+            }catch (InputMismatchException e){
+                System.out.println("Error.Debes poner solo numeros");
+                scanner.nextLine();
+            }
+        }while (continuar);
+        System.out.println("ID_cita:" + ID_cita + " fecha_cita:" + fecha_cita + " hora_cita:" + hora_cita);
+
+    }
+}
