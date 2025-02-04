e30ad2c921706976b85bebf348a5e698ced92c83
PedSemp <pedguisem@alu.edu.gva.es>
2025-02-04 08:53:24 +0100

    Cita

--- src/Cita/MainCita.java
+++ src/Cita/MainCita.java
@@ -16,29 +16,16 @@ public class MainCita {
             try {
                 System.out.print("Introduce el ID de la cita:");
                 ID_cita = scanner.nextInt();
-                continuar = false;
+                System.out.print("Introduce la fecha de la cita:");
+                fecha_cita= scanner.nextInt();
+                System.out.print("Introduce la hora de la cita");
+                hora_cita= scanner.nextInt();
+                continuar=false;
 
             } catch (InputMismatchException e) {
                 System.out.println("Error.Debes poner solo numeros");
                 scanner.nextLine();
 
-            }
-            try{
-                System.out.print("Introduce la fecha de la cita:");
-                fecha_cita= scanner.nextInt();
-                continuar=false;
-            }catch (InputMismatchException e){
-                System.out.println("Error.Debes poner solo numeros");
-                scanner.nextLine();
-            }
-            try{
-                System.out.print("Introduce la hora de la cita");
-                hora_cita= scanner.nextInt();
-                continuar=false;
-
-            }catch (InputMismatchException e){
-                System.out.println("Error.Debes poner solo numeros");
-                scanner.nextLine();
             }
         }while (continuar);
         System.out.println("ID_cita:" + ID_cita + " fecha_cita:" + fecha_cita + " hora_cita:" + hora_cita);
