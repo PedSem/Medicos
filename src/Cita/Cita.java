75090f2e1567295b15716f132fffed059686690d
PEDRO <pdguisem@alu.edu.gva.es>
2025-02-06 06:27:21 +0100

    Cita Medica

--- src/Cita/Cita.java
+++ src/Cita/Cita.java
@@ -6,11 +6,14 @@ public class Cita {
     protected int ID_cita;
     protected int fecha_cita;
     protected int hora_cita;
+    protected String diagnosis;
 
-    public Cita(int ID_cita,int fecha_cita, int hora_cita) {
+    public Cita(int ID_cita,int fecha_cita, int hora_cita,String diagnosis) {
         this.ID_cita = ID_cita;
         this.fecha_cita = fecha_cita;
         this.hora_cita = hora_cita;
+        this.diagnosis=diagnosis;
+
     }
 
     public int getID_cita() {
@@ -37,4 +40,11 @@ public class Cita {
         this.hora_cita = hora_cita;
     }
 
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
 }
--- src/Cita/MainCita.java
+++ src/Cita/MainCita.java
@@ -8,27 +8,63 @@ public class MainCita {
         int ID_cita=0;
         int fecha_cita=0;
         int hora_cita=0;
+        String diagnosis="";
         Scanner scanner=new Scanner(System.in);
-        boolean continuar=true;
+        boolean continuar=false;
 
         do {
-
             try {
                 System.out.print("Introduce el ID de la cita:");
                 ID_cita = scanner.nextInt();
-                System.out.print("Introduce la fecha de la cita:");
-                fecha_cita= scanner.nextInt();
-                System.out.print("Introduce la hora de la cita");
-                hora_cita= scanner.nextInt();
-                continuar=false;
+                continuar=true;
 
             } catch (InputMismatchException e) {
                 System.out.println("Error.Debes poner solo numeros");
                 scanner.nextLine();
 
             }
-        }while (continuar);
-        System.out.println("ID_cita:" + ID_cita + " fecha_cita:" + fecha_cita + " hora_cita:" + hora_cita);
+        }while (!continuar);
+        continuar=false;
+        do{
+            try{
+                System.out.print("Introduce la fecha de la cita:");
+                fecha_cita= scanner.nextInt();
+                continuar=true;
+            }catch (InputMismatchException e){
+                System.out.println("Error.Debes poner solo numeros");
+                scanner.nextLine();
+            }
+        }while (!continuar);
+        continuar=false;
+        do{
+            try{
+                System.out.print("Introduce la hora de la cita:");
+                hora_cita= scanner.nextInt();
+                continuar=true;
+            }catch (InputMismatchException e){
+                System.out.println("Error.Debes poner solo numeros");
+                scanner.nextLine();
+            }
+        }while (!continuar);
+        continuar=false;
+        do{
+            try{
+                System.out.print("Introduce el diagnosis:");
+                diagnosis= scanner.next();
+                for(int i=0;i<diagnosis.length();i++){
+                    if(Character.isLetter(diagnosis.charAt(i))){
+                        continuar=true;
+                    }else{
+                        System.out.println("Error.Solo se permite caracteres");
+                    }
+                }
+            }catch (NumberFormatException e){
+                System.out.println("Error.Solo se permite caracteres");
+                scanner.nextLine();
+            }
+        }while (!continuar);
+
+        System.out.println("ID_cita:" + ID_cita + " fecha_cita:" + fecha_cita + " hora_cita:" + hora_cita + " diagnosis:" + diagnosis);
 
     }
 }
